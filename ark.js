afc5a948b77b5c14807fdfb64d3cac10c503e9c3
Jamie Hoffmann <jamsarhoff@hotmail.com>
2018-01-11 01:11:21 +1100

    waiting events
    event has expanded dramatically --
    now the behaviour for a 'sleeping' event has been added and is contained within the event object. in this time the event will do nothing until it has counted its wait down.
    
    additionally, whether a character is present is now a field of that person object rather than a separate array (as makes sense).

--- ark.js
+++ ark.js
@@ -8,109 +8,170 @@ var game = (function() {
     var missingArray = [];
     var eventMap;
     
-    function getEventRecord(myCount) {
-        var iterateFunc = function() {
-            if (this.cur == 0) {
-                this.cur = this.count;
-                return true;
-            }
-            else {
-                this.cur--;
-                return false;
-            }
-        }
-        var advanceFunc = function() {
-            if (this.cur > 0) {
-                this.cur--;
-            }
-        }
-        var checkFunc = function() {
-            return this.cur;
-        }
-        
-        return {
-            ready: iterateFunc,
-            advance: advanceFunc,
-            check: checkFunc,
-            cur: 0,
-            count: myCount
-        }
-    }
-    
-    function getColorObject(myR, myG, myB) {
-        var avgFunc = function(other) { //define function once
-            var temp = getColorObject(other.r, other.g, other.b);
-            temp.r = Math.floor((this.r + temp.r) / 2);
-            temp.g = Math.floor((this.g + temp.g) / 2);
-            temp.b = Math.floor((this.b + temp.b) / 2);
-            return temp;
-        };
-        if (myR > 255 || myG > 255 || myB > 255 ||
-            myR < 0 || myG < 0 || myB < 0) {
-            return {
-                getAverage: avgFunc,
-                r: 255,
-                g: 255,
-                b: 255
-            };
-        }
-        return {
-            getAverage: avgFunc,
-            r: myR,
-            g: myG,
-            b: myB
-        };
-    }
-    
-    function getPersonObject(myName, myS, myC, myM, myP) {
-        var humourToIntFunc = function(val) {
-            if (val == "s") return 0;
-            else if (val == "c") return 1;
-            else if (val == "m") return 2;
-            else if (val == "p") return 3;
-            return 0;
-        }
-        var addHumourFunc = function(humour, amount)
-        {
-            console.log("Adding " + amount + " to " + humour);
-            var humourInt = this.humourToInt(humour);
-            
-            if (this.humours[humourInt] + amount > 150 || this.humours[humourInt] + amount < 0 || amount < 0) {
-                return; //fix this
-            }
-            this.humours[humourInt] += amount;
-            for (i = (humourInt + 1) % 4; amount > 0; i = (i + 1) % 4) {
-                if (i == humourInt) continue; //we dont subtract from the one we're adding to
-                if (this.humours[i] > 0) {
-                    this.humours[i]--;
-                    amount--;
+    return { //start of "ark" module functions
+        getEventRecord: function(myCount, options) {
+            if (options === undefined) options = {};
+            if (options.is_waiter === undefined) options.is_waiter = false;
+            if (options.wait_time === undefined) options.wait_time = 1;
+            if (options.starting_count === undefined) options.starting_count = 0;
+            var advanceFunc = function() { //counts down and returns true + resets when cur == 0
+                if (this.waiting()) {
+                    return false;
+                }
+                if (this.cur == 0) {
+                    this.cur = this.count;
+                    if (this.is_waiter) {
+                        this.waitStart();
+                    }
+                    return true;
+                }
+                else {
+                    this.cur--;
+                    return false;
                 }
             }
-        };
+            var stepFunc = function() { //counts down and returns true when cur == 0, but won't reset
+                if (this.waiting()) {
+                    return false;
+                }
+                if (this.cur > 0) {
+                    this.cur--;
+                    return false;
+                }
+                return true;
+            }
+            var readyFunc = function() { //returns whether the event is ready to trigger
+                if (this.waiting()) {
+                    return false;
+                }
+                return (this.cur == 0);
+            }
+            var checkFunc = function() { //returns the current number of counts left
+                return this.cur;
+            }
+            var waitStartFunc = function() { //starts the wait
+                if (this.is_waiter) {
+                    this.wait_cur = this.wait_max;
+                    this.waiting_status = true;
+                }
+            }
+            var waitingFunc = function() { //returns whether its waiting
+                return this.waiting_status;
+            }
+            var waitStepFunc = function() { //counts down the wait time and returns true when cur == 0, but wont reset
+                if (!this.waiting()) {
+                    return false;
+                }
+                if (this.wait_cur > 0) {
+                    this.wait_cur--;
+                    return false;
+                }
+                return true;
+            }
+            var waitAdvanceFunc = function() {
+                if (!this.waiting()) {
+                    return false;
+                }
+                if (this.wait_cur == 0) {
+                    this.wait_cur = this.wait_max;
+                    if (this.is_waiter) {
+                        this.waiting_status = false;
+                    }
+                    return true;
+                }
+                else {
+                    this.wait_cur--;
+                    return false;
+                }
+            }
+            
+            return {
+                advance: advanceFunc, //counts down and returns true + resets when cur == 0
+                step: stepFunc, //counts down and returns true when cur == 0, but won't reset
+                ready: readyFunc, //returns whether the event is ready to trigger
+                check: checkFunc,
+                waitStart: waitStartFunc,
+                waiting: waitingFunc,
+                waitAdvance: waitAdvanceFunc,
+                waitStep: waitStepFunc,
+                cur: options.starting_count,
+                count: myCount,
+                is_waiter: options.is_waiter,
+                wait_cur: options.wait_time,
+                wait_max: options.wait_time,
+                waiting_status: false
+            }
+        },
+    
+        getColorObject: function inner_name(myR, myG, myB) {
+            var avgFunc = function(other) { //define function once
+                var temp = inner_name(other.r, other.g, other.b);
+                temp.r = Math.floor((this.r + temp.r) / 2);
+                temp.g = Math.floor((this.g + temp.g) / 2);
+                temp.b = Math.floor((this.b + temp.b) / 2);
+                return temp;
+            };
+            if (myR > 255 || myG > 255 || myB > 255 ||
+                myR < 0 || myG < 0 || myB < 0) {
+                return {
+                    getAverage: avgFunc,
+                    r: 255,
+                    g: 255,
+                    b: 255
+                };
+            }
+            return {
+                getAverage: avgFunc,
+                r: myR,
+                g: myG,
+                b: myB
+            };
+        },
         
-        if (myS < 0 || myC < 0 || myM < 0 || myP < 0) {
+        getPersonObject: function(myName, myS, myC, myM, myP) {
+            var humourToIntFunc = function(val) {
+                if (val == "s") return 0;
+                else if (val == "c") return 1;
+                else if (val == "m") return 2;
+                else if (val == "p") return 3;
+                return 0;
+            }
+            var addHumourFunc = function(humour, amount)
+            {
+                console.log("Adding " + amount + " to " + humour);
+                var humourInt = this.humourToInt(humour);
+                
+                if (this.humours[humourInt] + amount > 150 || this.humours[humourInt] + amount < 0 || amount < 0) {
+                    return; //fix this
+                }
+                this.humours[humourInt] += amount;
+                for (i = (humourInt + 1) % 4; amount > 0; i = (i + 1) % 4) {
+                    if (i == humourInt) continue; //we dont subtract from the one we're adding to
+                    if (this.humours[i] > 0) {
+                        this.humours[i]--;
+                        amount--;
+                    }
+                }
+            };
+            
+            if (myS < 0 || myC < 0 || myM < 0 || myP < 0) {
+                return {
+                    humourToInt: humourToIntFunc,
+                    addHumour: addHumourFunc,
+                    name: myName,
+                    humours: [38, 38, 37, 37]
+                };
+            }
             return {
                 humourToInt: humourToIntFunc,
                 addHumour: addHumourFunc,
                 name: myName,
-                humours: [38, 38, 37, 37]
+                humours: [myS, myC, myM, myP],
+                present: true
             };
-        }
-        return {
-            humourToInt: humourToIntFunc,
-            addHumour: addHumourFunc,
-            name: myName,
-            humours: [myS, myC, myM, myP]
-        };
-    }
+        },
     
-    return { //start of "ark" module functions
-        
         gameInit: function() {
-            this.eventMap = {
-                "uranosCoolLvl1": getEventRecord(10),
-                "aresMadLvl1": getEventRecord(5)
-            }
             jhc.clearInput();
             jhc.clearOutput();
             jhc.outputLine(game_name + "<br>A long-term journey through space.<br>" +
@@ -119,21 +180,22 @@ var game = (function() {
             "Glancing around, you see the familiar faces of Hermes, Aphrodite, Ares, Kronos, Uranos and Poseidon.<br>" +
             "May the gods help you all.<br>" +
             "**********************************<br>");
-            colorS = getColorObject(255, 0, 0);
-            colorC = getColorObject(127, 127, 0);
-            colorM = getColorObject(0, 0, 255);
-            colorP = getColorObject(0, 255, 0);
-            this.personArray = [getPersonObject("Gaia",       0,     25,    100,     25),
-                                getPersonObject("Hermes",    25,     50,     25,     50),
-                                getPersonObject("Aphrodite", 75,     25,     25,     25),
-                                getPersonObject("Ares",      25,    100,     25,      0),
-                                getPersonObject("Kronos",    25,     75,     25,     25),
-                                getPersonObject("Uranos",   100,     25,      0,     25),
-                                getPersonObject("Poseidon",  25,      0,     25,    100)];
-            this.missingArray = [getPersonObject("David",     0,      0,      0,      0),
-                                 getPersonObject("Jamie",   255,    255,    255,    255)];
+            colorS = this.getColorObject(255, 0, 0);
+            colorC = this.getColorObject(127, 127, 0);
+            colorM = this.getColorObject(0, 0, 255);
+            colorP = this.getColorObject(0, 255, 0);
+            this.personArray = [this.getPersonObject("Gaia",       0,     25,    100,     25),
+                                this.getPersonObject("Hermes",    25,     50,     25,     50),
+                                this.getPersonObject("Aphrodite", 1,     25,     25,     25),
+                                this.getPersonObject("Ares",      25,    100,     25,      0),
+                                this.getPersonObject("Kronos",    25,     75,     25,     25),
+                                this.getPersonObject("Uranos",   100,     25,      0,     25),
+                                this.getPersonObject("Poseidon",  25,      0,     25,    100)];
+            this.initEvents();
             this.genBackground();
         },
+        
+        initEvents: 0,
 
         gameInput: function() {
             var input_line = jhc.getInputLine();
@@ -152,34 +214,40 @@ var game = (function() {
                         A set of dice and a cup have been placed at the centre of the table.\
                         Looking around, you see ";
             //compile lists of present and missing people
+            var missingArray = [];
             var present_names = "";
             for (var i = 1; i < this.personArray.length - 1; i++) {
-                present_names = present_names + this.personArray[i].name;
-                if (this.personArray.length > 2) {
-                    present_names = present_names + ", "; //these are so that the commas are correct
+                if (this.personArray[i].present) {
+                    present_names = present_names + this.personArray[i].name;
+                    if (this.personArray.length > 2) {
+                        present_names = present_names + ", "; //these are so that the commas are correct
+                    }
+                    else {
+                        present_names = present_names + " "; //as above
+                    }
                 }
                 else {
-                    present_names = present_names + " "; //as above
+                    missingArray.push(this.personArray[i]);
                 }
             }
             present_names = present_names + "and " + this.personArray[this.personArray.length - 1].name + ". ";
             desc = desc + present_names;
-            if (this.missingArray.length > 1) {
+            if (missingArray.length > 1) {
                 var missing_names = "";
-                for (var i = 0; i < this.missingArray.length - 1; i++) {
+                for (var i = 0; i < missingArray.length - 1; i++) {
                     missing_names = missing_names + this.missingArray[i].name;
-                    if (this.missingArray.length > 2) {
+                    if (missingArray.length > 2) {
                         missing_names = missing_names + ", ";
                     }
                     else {
                         missing_names = missing_names + " ";
                     }
                 }
-                missing_names = missing_names + "and " + this.missingArray[this.missingArray.length - 1].name + " ";
+                missing_names = missing_names + "and " + missingArray[missingArray.length - 1].name + " ";
                 desc = desc + missing_names + "are conspicuously absent.";
             }
-            else if (this.missingArray.length == 1) {
-                desc = desc + this.missingArray[0].name + " is conspicuously absent.";
+            else if (missingArray.length == 1) {
+                desc = desc + missingArray[0].name + " is conspicuously absent.";
             }
             return desc;
         },
@@ -246,12 +314,12 @@ var game = (function() {
             humours[2] /= 255;
             humours[3] /= 255;
             //now we average all the colours and blend with grey for nice pastel tones
-            var bg = getColorObject(127, 127, 127);
-            bg = bg.getAverage(getColorObject(colorS.r * humours[0], colorS.g * humours[0], colorS.b * humours[0]));
-            bg = bg.getAverage(getColorObject(colorC.r * humours[1], colorC.g * humours[1], colorC.b * humours[1]));
-            bg = bg.getAverage(getColorObject(colorM.r * humours[2], colorM.g * humours[2], colorM.b * humours[2]));
-            bg = bg.getAverage(getColorObject(colorP.r * humours[3], colorP.g * humours[3], colorP.b * humours[3]));
-            bg = bg.getAverage(getColorObject(150, 150, 150));
+            var bg = this.getColorObject(127, 127, 127);
+            bg = bg.getAverage(this.getColorObject(colorS.r * humours[0], colorS.g * humours[0], colorS.b * humours[0]));
+            bg = bg.getAverage(this.getColorObject(colorC.r * humours[1], colorC.g * humours[1], colorC.b * humours[1]));
+            bg = bg.getAverage(this.getColorObject(colorM.r * humours[2], colorM.g * humours[2], colorM.b * humours[2]));
+            bg = bg.getAverage(this.getColorObject(colorP.r * humours[3], colorP.g * humours[3], colorP.b * humours[3]));
+            bg = bg.getAverage(this.getColorObject(150, 150, 150));
             this.setBackground(Math.floor(bg.r), Math.floor(bg.g), Math.floor(bg.b));
         },
         
--- events.js
+++ events.js
@@ -1,3 +1,15 @@
+game.initEvents = function() {
+    this.eventMap = {
+        //simple mood expressions
+        "uranosCoolLvl1": this.getEventRecord(10),
+        "aresMadLvl1": this.getEventRecord(5),
+        "poseidonPatientLvl1": this.getEventRecord(4),
+        "poseidonPatientLvl2": this.getEventRecord(4),
+        //ares storyline
+        "aresStormOut": this.getEventRecord(4, {is_waiter: true, wait_time: 10, starting_count: 4})
+    }
+}
+
 game.procTemperament = function(t_val) {
     this.personArray[0].addHumour(t_val, 10);
     //if the updated humour goes over 127, everyone starts to feel it
@@ -7,38 +19,55 @@ game.procTemperament = function(t_val) {
             this.personArray[i].addHumour(t_val, 5);
         }
     }
+    
+    if (this.eventMap["aresStormOut"].waitAdvance()) { //this will trigger when the wait ends and no other time
+        this.personArray[3].present = true;
+    }
     //specific character interactions below
-    if (t_val == "s") {
-        for (i = 0; i < this.personArray.length; i++) {
-            if (i != 3) {
-                this.personArray[i].addHumour(t_val, 5);
-            }
-        }
-        
-        if (this.personArray[5].humours[3] > 120) {
-            if (this.eventMap["uranosCoolLvl1"].ready()) {
-                jhc.outputLine("Uranos meets your gaze.");
+    if (t_val == "s") {    
+        if (this.personArray[5].present) {
+            if (this.personArray[5].humours[3] > 120) {
+                if (this.eventMap["uranosCoolLvl1"].advance()) {
+                    jhc.outputLine("Uranos meets your gaze.");
+                }
             }
         }
     }
     else if (t_val == "c") {
-        this.personArray[3].addHumour(t_val, 10);
-        if (this.personArray[3].humours[1] > 125) {
-            if (this.eventMap["aresMadLvl1"].ready()) {
-                jhc.outputLine("Ares looks murderous.");
+        if (this.personArray[3].present) {
+            this.personArray[3].addHumour(t_val, 10);
+            if (this.personArray[3].humours[1] > 110) {
+                if (this.eventMap["aresMadLvl1"].advance()) {
+                    jhc.outputLine("Ares looks murderous.");
+                    this.eventMap["aresStormOut"].step();
+                }
+            }
+            if (this.personArray[3].humours[1] > 120 && //ares high chol
+                     this.personArray[2].humours[0] < 30 && //aphro low sang
+                     this.eventMap["aresStormOut"].ready()) {
+                this.personArray[3].present = false;
+                jhc.outputLine("Ares stands up, his hands balled into tight fists\
+                                and a look of rage and frustration on his face. He storms out\
+                                loudly.");
+                this.eventMap["aresStormOut"].advance();
             }
         }
+        
     }
     else if (t_val == "m") {
     
     }
     else if (t_val == "p") {
-        this.personArray[6].addHumour(t_val, 10);
-        if (this.personArray[6].humours[3] > 110) {
-            jhc.outputLine("Poseidon's patience seems never-ending.");
-        }
-        if (this.personArray[6].humours[3] > 135) {
-            jhc.outputLine("Is Poseidon even paying attention?");
+        if (this.personArray[6].present) {
+            this.personArray[6].addHumour(t_val, 10);
+            if (this.personArray[6].humours[3] > 110 && this.eventMap["poseidonPatientLvl1"].advance()) {
+                jhc.outputLine("Poseidon's patience seems never-ending.");
+                this.eventMap["poseidonPatientLvl2"].step();
+            }
+            else if (this.personArray[6].humours[3] > 135 && this.eventMap["poseidonPatientLvl2"].ready()) {
+                jhc.outputLine("Is Poseidon even paying attention?");
+                this.eventMap["poseidonPatientLvl2"].advance(); //don't want it repeating
+            }
         }
     }
 }
